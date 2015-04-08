85e89a223d2c298152c068070b93d83f2cb94ef6
talamoig <ivano.talamo@gmail.com>
2015-04-08 10:31:57 +0200

    added domainremove option

--- README.md
+++ README.md
@@ -8,12 +8,7 @@ The basic idea is that:
  - the `environment` of a node in Puppet is the `state` (eg. `production`, `new`, `dev`, `retired`) of the computer in GLPI;
  - `role` in Puppet is a [custom field](http://www.glpi-project.org/wiki/doku.php?id=en:plugins:customfields_use) in GLPI (eg. `puppetrole`).
 
-If you have a running GLPI with the *custom fields* plugin:
-
- - installed
- - enabled
- - a custom field defined on Computers with `puppetrole` as System Name
-
+If you have a running GLPI with the *custom fields* plugin,
 the necessary informations for the given host `hostname` can be extracted directly from the GLPI database with the following query:
 
     SELECT glpi_computers.name,
@@ -25,6 +20,7 @@ the necessary informations for the given host `hostname` can be extracted direct
      WHERE glpi_states.id=glpi_computers.states_id
        AND glpi_computers.name='hostname'
        AND glpi_plugin_customfields_computers.id=glpi_computers.id;
+
 
 ### Requirements
 
--- glpienc.py
+++ glpienc.py
@@ -1,31 +1,43 @@
 #!/bin/env python
 import MySQLdb
 import sys
-
-execfile('config.py')
+import os
 
 if len(sys.argv)!=2:
-    print "Usage: %s <hostname>"%sys.argv[0]
+    sys.stderr.write("Usage: %s <hostname>\n"%sys.argv[0])
     sys.exit(1)
 
-host=sys.argv[1]
+configfiles=['/etc/glpienc/config.py','config.py']
+
+configured=False
+for conffile in configfiles:
+    if os.path.isfile(conffile):
+        configured=True
+        execfile(conffile)
+        break
+
+if not configured:
+    files=' '.join(configfiles)
+    sys.stderr.write("Could not ready any of the following configuration files: %s\n"%files)
+    sys.exit(1)
 
 db = MySQLdb.connect(host=dbhost, # your host, usually localhost
                      user=dbuser, # your username
-                      passwd=dbpasswd, # your password
-                      db=dbname) # name of the data base
+                     passwd=dbpasswd, # your password
+                     db=dbname) # name of the data base
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
 cur = db.cursor() 
+
+host=sys.argv[1]
+if domainremove:
+    host=host.split('.')[0]
 
 query="SELECT glpi_computers.name,glpi_states.name,glpi_plugin_customfields_computers.%s \
 FROM glpi_plugin_customfields_computers,glpi_computers,glpi_states \
 WHERE glpi_states.id=glpi_computers.states_id \
 AND glpi_computers.name='%s' \
 AND glpi_plugin_customfields_computers.id=glpi_computers.id"%(puppetrole,host)
-#print query
-#sys.exit(0)
+
 cur.execute(query)
 
 # print all the first cell of all the rows
@@ -35,7 +47,7 @@ if len(rows)!=1:
     sys.stderr.write("query returned %s results. Expecting 1\n"%len(rows))
     sys.exit(1)
 host=rows[0][0]
-env=rows[0][1]
+env=rows[0][1].replace(' ','-')
 role=rows[0][2]
 print "---"
 print "   environment: %s"%env
